c3b1b8a3793ab71f999f729ea12a6bee06ea880d
szhayes <51539583+szhayes@users.noreply.github.com>
2021-08-26 14:46:55 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
  # source  = "tfe.shayes.3607853379.workshops.arctiq.ca/arctiq/gke/google"
  # source  = "34.75.63.58/arctiq/gke/google"
  source       = "git::https://github.com/szhayes/terraform-google-gke.git?ref=1.0.2"
- version = "1.0.0"
+ # version = "1.0.0"
 
  # insert required variables here
  cluster_name = var.cluster_name
