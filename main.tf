ce4e33483d9ef34a99861e75b869adc5405120be
szhayes <51539583+szhayes@users.noreply.github.com>
2021-08-26 14:01:51 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
  module "gke" {
- source  = "tfe.shayes.3607853379.workshops.arctiq.ca/arctiq/gke/google"
+ # source  = "tfe.shayes.3607853379.workshops.arctiq.ca/arctiq/gke/google"
  # source  = "34.75.63.58/arctiq/gke/google"
+ source       = "git::https://github.com/szhayes/terraform-google-gke.git?ref=1.0.2"
  version = "1.0.0"
 
  # insert required variables here
