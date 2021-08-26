78a7d15091f18a20b128ff290d81a5da65e18f91
Stuart Hayes <stuart.hayes@teranet.ca>
2021-08-26 14:58:02 +0000

    initial gke terraform project push

---
+++ main.tf
@@ -0,0 +1,9 @@
+ module "gke1" {
+ source  = "tfe.shayes.3607853379.workshops.arctiq.ca/arctiq/gke/google"
+ version = "1.0.0"
+
+ # insert required variables here
+ cluster_name = var.cluster_name
+ project_id=var.gcp_project_id
+
+ }
---
+++ variables.tf
@@ -0,0 +1,2 @@
+ variable "cluster_name" {}
+ variable "project_id" {}
